2d01bcf25ede2250908bec18ab551076290d2050
JerryWins1 <jerrywins1@gmail.com>
2026-09-03 20:49:03 -0500

    update flipkit/sw.js

---
+++ flipkit/sw.js
@@ -0,0 +1,4 @@
+const CACHE='flipkit-v1';
+self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])).then(()=>self.skipWaiting())); });
+self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k.startsWith('flipkit-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
+self.addEventListener('fetch',e=>{ if(e.request.method!=='GET') return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{ if(new URL(e.request.url).origin===location.origin){ const cp=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,cp)); } return res; }).catch(()=>caches.match('./index.html')))); });
