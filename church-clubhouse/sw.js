badcbe902471d74b88cf885d03548e32400f85d1
JerryWins1 <jerrywins1@gmail.com>
2026-09-10 08:40:39 -0500

    bump cache to church-clubhouse-v17

--- church-clubhouse/sw.js
+++ church-clubhouse/sw.js
@@ -1,4 +1,4 @@
-const CACHE='church-clubhouse-v16';
+const CACHE='church-clubhouse-v17';
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])).then(()=>self.skipWaiting())); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k.startsWith('church-clubhouse-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
 self.addEventListener('fetch',e=>{ if(e.request.method!=='GET') return; if(!e.request.url.startsWith(location.origin)) return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{ const cp=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,cp)); return res; }).catch(()=>caches.match('./index.html')))); });
