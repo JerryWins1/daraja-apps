1cd3eb8ce2272868dca48149fd29520daca56665
JerryWins1 <jerrywins1@gmail.com>
2026-09-22 16:33:46 -0500

    bump cache to everyday-v23

--- everyday/sw.js
+++ everyday/sw.js
@@ -1,4 +1,4 @@
-const CACHE='everyday-v22';
+const CACHE='everyday-v23';
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])).then(()=>self.skipWaiting())); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k.startsWith('everyday-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
 self.addEventListener('fetch',e=>{ if(e.request.method!=='GET') return; e.respondWith(fetch(e.request).then(r=>{ const c=r.clone(); caches.open(CACHE).then(x=>x.put(e.request,c)); return r; }).catch(()=>caches.match(e.request))); });
